a4a43c209625da550d78cd35592c5cc74d7d7e2f
arronf2e <191446367@qq.com>
2021-04-28 08:42:42 +0800

    update demo3

--- src/demo2.ts
+++ src/demo2.ts
@@ -10,3 +10,7 @@ let arr4: NumberArray = [1,2,3]
 let arron: [string, number, boolean] = ['arron', 28, true]
 
 console.log(arron[1])
+
+let tuple1: (number | string)[] = [1,2,'123']
+tuple1.push('4')
+console.log(typeof tuple1);
---
+++ src/demo3.ts
@@ -0,0 +1,6 @@
+// x,y都为number
+function sum(x: number, y: number): number {
+    return x + y
+}
+
+sum(1, 2)
